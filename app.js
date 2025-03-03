9da4b01b312484b928fe13a072ad7cb988b02183
AbdumalikovMuhammadqodir <abdumalikov3011@gmail.com>
2025-03-04 01:56:46 +0900

    feat: FrontEnd javasckripni qurush

--- app.js
+++ app.js
@@ -30,14 +30,9 @@ app.set("view engine", "ejs");
 app.post("/create-item", (req, res) => {
   console.log("user entered /create-item");
   const new_reja = req.body.reja;
-  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end(`something went wrong`);
-    } else {
-      res.end(`successfunlly eddeb`);
-    }
-  });
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {});
+  console.log(data.ops);
+  res.json(data.ops[0]);
 });
 
 // app.get("/gift", function (req, res) {
---
+++ public/author/browser.js
@@ -0,0 +1,39 @@
+console.log("FrontEnd JS ishga tushdi");
+
+function itemTemplate(item) {
+    return `<li
+            class="list-group-item list-group-info d-flex ligin-items center justify-content-between"
+          >
+            <span class="item-text">${item.reja}</span>
+            <div>
+              <button
+                data-id="${item_id}"
+                class="edit-me btn btn-secondary btn-sm mr-1"
+              >
+                Ozgartirish
+              </button>
+              <button
+                data-id="${item_id}"
+                class="delete-me btn btm-danger btn-sm"
+              >
+                Ochirish
+              </button>
+            </div>
+          </li>`;
+}
+
+let createField = document.getElementById("create-field");
+
+document.getElementById("create-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  axios
+    .post("/create-item", { reja: new_reja.value })
+    .then((response) => {
+      document.getElementById("item-list")this.insertAdjacentHTML("beforeend". itemTemplate(data))
+      createField.value = "";
+      createField.focus();
+    })
+    .catch((err) => {});
+    console.log("Iltumos qaytadan harakat qiling");
+});
